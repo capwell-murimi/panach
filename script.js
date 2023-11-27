b4f4dfcc6f8526a0062e00f12afc81f3f2846973
Capwell Murimi <capwellmurimi@gmail.com>
2023-11-27 21:56:59 +0300

    new stuff

--- script.js
+++ script.js
@@ -5,22 +5,29 @@ const words = [
     'Translation',
     'Voice Overs',
     'Transcription'
-  ];
-  
-        let currentWordIndex = 0;
+];
 
-        function animateWords() {
-            const wordAnimation = document.getElementById('wordAnimation');
-            wordAnimation.innerHTML = words[currentWordIndex];
-            // Generate a random color in hex format
-            const randomColor = '#' + Math.floor(Math.random()*10111111).toString(16);
-            wordAnimation.style.color = randomColor;
+const colors = [
+    '#3498db', // Blue
+    '#f39c12', // Orange
+    '#2ecc71', // Green
+    '#f39c12', // Orange
+    '#2ecc71', // green
+    '#1abc9c'  // Teal
+];
 
-            currentWordIndex = (currentWordIndex + 1) % words.length;
+let currentWordIndex = 0;
 
-            setTimeout(animateWords, 1000);
-        }
+function animateWords() {
+    const wordAnimation = document.getElementById('wordAnimation');
+    wordAnimation.innerHTML = words[currentWordIndex];
+    wordAnimation.style.color = colors[currentWordIndex];
 
-        document.addEventListener('DOMContentLoaded', animateWords);
+    currentWordIndex = (currentWordIndex + 1) % words.length;
+
+    setTimeout(animateWords, 1000);
+}
+
+document.addEventListener('DOMContentLoaded', animateWords);
 
 
