382df105638aebfafc6c028ffe38f6b1db23b95d
Capwell Murimi <capwellmurimi@gmail.com>
2023-12-01 20:41:35 +0300

    new stuff

--- script.js
+++ script.js
@@ -32,3 +32,25 @@ function animateWords() {
 document.addEventListener('DOMContentLoaded', animateWords);
 
 
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll('nav a');
+  
+    navLinks.forEach(link => {
+      link.addEventListener('click', smoothScroll);
+    });
+  
+    function smoothScroll(e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+  
+      window.scrollTo({
+        top: targetElement.offsetTop - 50, // Adjust for header height
+        behavior: 'smooth'
+      });
+    }
+  });
+  
